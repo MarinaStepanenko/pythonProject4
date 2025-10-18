0423e2e24b67b2f880d4249615a2b36205105c2f
Marina Stepanenko <m4liwka007@yandex.ru>
2025-10-18 23:04:01 +0300

    добавление логеров в файлы masks.py и utils.py, создание папки logs

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,32 @@
 import json
+import logging
 import os.path
 
 
-def get_operations(directory: str) -> list[dict]:
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/utils.log", encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
+def get_operations(directory: str) -> list[dict] | str:
     """
     Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
     """
-    if not os.path.exists(directory):
-        return []
-    with open(directory, "r", encoding="utf-8") as f:
-        operations = json.load(f)
-        if not isinstance(operations, list):
+    try:
+        if not os.path.exists(directory):
+            logger.warning(f"Директория файла {directory} не найдена")
             return []
-    return operations
+        with open(directory, "r", encoding="utf-8") as f:
+            logger.info(f"Информация о транзакциях была выгружена из json файла {directory}.")
+            operations = json.load(f)
+            if not isinstance(operations, list):
+                logger.warning(f"{directory} не содержит список")
+                return []
+            return operations
+    except Exception as ex:
+        logger.error(f"Произошла ошибка: {ex}")
+        return f"Ошибка: {ex}"
--- tests/test_get_convertation.py
+++ tests/test_get_convertation.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock, patch, Mock
+from unittest.mock import MagicMock, Mock, patch
 
 from src.external_api import get_convertation
 
